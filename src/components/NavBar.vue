de5438c4c721e2adba21ceb48d48b41c3fc9417b
peaw1998 <m.peaw5500@gmail.com>
2020-06-24 05:07:15 +0700

    refactor navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,14 +3,32 @@
     <v-btn v-on:click="goToHome" depressed small color="primary"
       >ALL ALBUMS</v-btn
     >
+    <v-btn v-on:click="prevPage" depressed small color="primary"
+      >PREV ALBUM</v-btn
+    >
+    <v-btn v-on:click="nextPage" depressed small color="primary"
+      >NEXT ALBUM</v-btn
+    >
   </v-app-bar>
 </template>
 <script>
 import router from "../router/index";
+import { mapActions, mapGetters } from "vuex";
 export default {
+  computed: {
+    ...mapGetters(["getAlbumID"]),
+  },
   methods: {
+    ...mapActions(["fetchNextAlbumID", "fetchPrevAlbumID", "clearState"]),
+    nextPage: function() {
+      this.fetchNextAlbumID();
+    },
+    prevPage: function() {
+      this.fetchPrevAlbumID();
+    },
     goToHome: function() {
       router.push("/");
+      this.clearState();
     },
   },
 };
--- src/store/modules/photos.js
+++ src/store/modules/photos.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const state = {
   photos: [],
   photobyAlbumID: [],
+  albumID: 0,
 };
 
 const getters = {
@@ -11,6 +12,9 @@ const getters = {
   },
   getPhotobyAlbumID: (state) => {
     return state.photobyAlbumID;
+  },
+  getAlbumID: (state) => {
+    return state.albumID;
   },
 };
 
@@ -27,6 +31,23 @@ const actions = {
       const array = state.photos.filter((item) => item.albumId == id);
       commit("setPhotobyAlbumIDMutation", array);
     });
+    commit("setAlbumIDMutation", id);
+  },
+  fetchNextAlbumID: ({ commit }) => {
+    if (state.albumID + 1 <= 100)
+      commit("setAlbumIDMutation", state.albumID + 1);
+    const array = state.photos.filter((item) => item.albumId == state.albumID);
+    commit("setPhotobyAlbumIDMutation", array);
+  },
+  fetchPrevAlbumID: ({ commit }) => {
+    if (state.albumID - 1 > 0) commit("setAlbumIDMutation", state.albumID - 1);
+    const array = state.photos.filter((item) => item.albumId == state.albumID);
+    commit("setPhotobyAlbumIDMutation", array);
+  },
+  clearState: ({ commit }) => {
+    commit("setPhotosMutation", []);
+    commit("setPhotobyAlbumIDMutation", []);
+    commit("setAlbumIDMutation", 0);
   },
 };
 
@@ -36,6 +57,9 @@ const mutations = {
   },
   setPhotobyAlbumIDMutation: (state, value) => {
     state.photobyAlbumID = value;
+  },
+  setAlbumIDMutation: (state, value) => {
+    state.albumID = value;
   },
 };
 
