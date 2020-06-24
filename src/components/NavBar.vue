443c261dc85d33785e6a7a812e89252360f78583
peaw1998 <m.peaw5500@gmail.com>
2020-06-24 13:37:28 +0700

    edit next and prev button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,10 +3,21 @@
     <v-btn v-on:click="goToHome" depressed small color="primary"
       >ALL ALBUMS</v-btn
     >
-    <v-btn v-on:click="prevPage" depressed small color="primary"
+    <v-btn
+      v-on:click="prevPage"
+      v-bind:disabled="disabledPrev()"
+      depressed
+      small
+      color="primary"
       >PREV ALBUM</v-btn
     >
-    <v-btn v-on:click="nextPage" depressed small color="primary"
+
+    <v-btn
+      v-on:click="nextPage"
+      v-bind:disabled="disabledNext()"
+      depressed
+      small
+      color="primary"
       >NEXT ALBUM</v-btn
     >
   </v-app-bar>
@@ -25,6 +36,16 @@ export default {
     },
     prevPage: function() {
       this.fetchPrevAlbumID();
+    },
+    disabledNext: function() {
+      if (this.getAlbumID === 99) return true;
+      if (this.getAlbumID === 0) return true;
+      else false;
+    },
+    disabledPrev: function() {
+      if (this.getAlbumID === 1) return true;
+      if (this.getAlbumID === 0) return true;
+      else false;
     },
     goToHome: function() {
       router.push("/");
